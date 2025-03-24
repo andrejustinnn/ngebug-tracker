0bae3c08563882d9185afa8699b271cfa2f3bf6c
Andre Justin <andrejustin77@gmail.com>
2025-03-24 19:25:07 +0700

    confirmation dialog on delete issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,15 +1,36 @@
-import { Button } from '@/components/ui/button'
-import { TrashIcon } from '@radix-ui/react-icons'
-import Link from 'next/link'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
 
 const DeleteIssueButton = ({issueId} : {issueId:number}) => {
   return (
-    <Button className='w-full' variant="outline">
-      <TrashIcon />
-      <Link href={`/issues/${issueId}/delete`}>
-        Delete Issue
-      </Link>
-    </Button>
+    <AlertDialog>
+    <AlertDialogTrigger asChild>
+      <Button variant="outline" className="w-full">Delete Issue</Button>
+    </AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete your
+          account and remove your data from our servers.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction>Continue</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
   )
 }
 
