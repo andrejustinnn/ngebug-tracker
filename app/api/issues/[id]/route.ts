357e2014b56d7c8383485fd1b416fdcc7258e028
Andre Justin <andrejustin77@gmail.com>
2025-05-10 08:34:07 +0700

    enhance api patch to assign issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { issueSchema } from "@/app/validationSchema";
+import { issueSchema, patchIssueSchema } from "@/app/validationSchema";
 import { prisma } from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -13,9 +13,21 @@ export async function PATCH (request: NextRequest, {params} : {params: {id: stri
   }
   const body = await request.json();
 
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if(!validation.success) return NextResponse.json(validation.error.format(), {status: 400});
+
+  const {title, description, assignedToUserId} = body
+
+  if(assignedToUserId){
+    const user = await prisma.user.findUnique({
+      where: {
+        id: assignedToUserId
+      }
+    })
+
+    if(!user) return NextResponse.json({error: 'User not found.'}, {status: 400});
+  }
 
   const issue = await prisma.issue.findUnique({
     where: {
@@ -28,8 +40,10 @@ export async function PATCH (request: NextRequest, {params} : {params: {id: stri
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description
+      title,
+      description,
+      assignedToUserId
+      // ketika salah satu ini undefined, gak akan ke update menjadi null atau undefined 
     }
   });
 
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -2,6 +2,15 @@ import { z } from "zod";
 
 export const issueSchema = z.object({
   title: z.string().min(1, "Title is required.").max(255),
-  description: z.string().min(1, "Description is required."),
+  description: z.string().min(1, "Description is required.").max(65535)
   // kosongkan second argument untuk menggunakan pesan default
+});
+
+// untuk case misalnya kita hanya mau update assigned to user id atau statusnya saja. 
+// kalau kita pakai schema "issueSchema", saat ingin update status misalnya kita harus mencantumkan title and desc krna required
+// maka kita harus buat schema baru untuk patching
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Title is required.").max(255).optional(),
+  description: z.string().min(1, "Description is required.").max(65535).optional(),
+  assignedToUserId: z.string().min(1, "Assigned to user is required").max(255).optional().nullable(),
 });
