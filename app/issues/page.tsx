b4fe81466fbe18fb9125fe1fd4705b0968cb11cd
Andre Justin <andrejustin77@gmail.com>
2025-05-19 21:00:14 +0700

    sorting data

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -39,10 +39,18 @@ const IssuesPage = async ({searchParams}: Props) => {
 
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined; // undefined agar prisma tidak anggap filtering ini
+
+  // valid orderby 
+  const isValidOrderBy = columns.map(column => column.value).includes(searchParams.orderBy);
+  const orderBy = isValidOrderBy ? {
+    [searchParams.orderBy]: searchParams.sort === 'ASC' ? 'asc' : 'desc'
+  } : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status
-    }
+    },
+    orderBy: orderBy,
   });
   const sortControl = (orderBy: keyof Issue) => {
     
